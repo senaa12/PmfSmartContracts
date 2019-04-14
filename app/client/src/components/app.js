155e4a5b7565c55b899e1bff6a429dc0e74fdb18
sena12 <luka.senicic@gmail.com>
2019-04-14 18:04:33 +0200

    example init (commit before resolving eslint)

---
+++ app/client/src/components/app.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from "react";
+
+import "./app.scss";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My React App!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ app/client/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({ template: "./src/index.html" });
+
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [ htmlWebpackPlugin ]
+};
